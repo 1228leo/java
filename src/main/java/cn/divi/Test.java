3de87776329cc646b07f23b646952a34982b350a
leo <740034600@qq.com>
2020-04-01 14:07:37 +0800

    init

---
+++ src/main/java/cn/divi/Test.java
@@ -0,0 +1,11 @@
+package cn.divi;
+
+/**
+ * Create : 2020/4/1 14:00
+ * Create By Leo
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
